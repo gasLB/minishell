d445af9c922289793be9b4b1bb4b4531cbb68a17
g <gaston.fontagneu@gmail.com>
2025-06-06 12:24:35 +0200

    default exit status with exit is last_exit

--- src/builtins2.c
+++ src/builtins2.c
@@ -64,7 +64,7 @@ void	ft_exit(int ac, char **args, t_minishell *sh)
 {
 	ft_printf("exit\n");
 	if (ac == 0)
-		exit_with_number(2, args, sh);
+		exit_with_number(sh->last_exit, args, sh);
 	else if (!is_a_number(args[1]) || !is_correct_size_exit(args[1]))
 	{
 		printf_fd(2, "minishell: exit: %s: " NUMERIC_ARG, args[1]);
